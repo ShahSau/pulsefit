0526cfc4a7af6bffe2f77c62a2ee3ef1f1612efa
shahriar <shahriarksaurov@gmail.com>
2024-08-28 11:35:48 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-blue-800 text-rose-950'>
+        iddjdudjj
+      </div>
+    </>
+  )
+}
+
+export default App
